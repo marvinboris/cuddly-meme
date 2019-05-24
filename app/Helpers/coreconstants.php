f3d8138426736602a6a9ee17864352d478143310
sdmg15 <sonkengmaldini@gmail.com>
2019-05-24 22:52:44 +0100

    Base for transactions + coreconstants

---
+++ app/Helpers/coreconstants.php
@@ -0,0 +1,8 @@
+<?php
+
+const PAYMENT_SUCCESS_STATUS = 1;
+const PAYMENT_FAILURE_STATUS = 2;
+
+const PAYMENT_CANCELLED_TEXT = 'cancelled';
+const PAYMENT_SUCCESS_TEXT = 'success';
+const PAYMENT_FAILED_TEXT = 'failed';
