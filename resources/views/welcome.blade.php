27d62768a63ba652670c8ae84c06b6d87102aa34
sdmg15 <sonkengmaldini@gmail.com>
2019-05-24 11:19:13 +0100

    Setting base front view with dump elements

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,99 +1,282 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends("partials.layout")
 
-        <title>Laravel</title>
+@section("content")
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+@include("partials.nav")
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
+    <section class="category section bg-gray">
+        <div class="container">
+            <div class="section-header">
+                <h2 class="section-title">Browse Categories</h2>
+                <p>Most popular categories of portal, sorted by popularity</p>
+            </div>
+            <div class="row">
+                <div class="col-lg-3 col-md-6 col-xs-12 f-category">
+                    <a href="browse-jobs.html">
+                        <div class="icon bg-color-1">
+                            <i class="lni-home"></i>
+                        </div>
+                        <h3>Finance</h3>
+                        <p>(4286 jobs)</p>
+                    </a>
                 </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+                <div class="col-lg-3 col-md-6 col-xs-12 f-category">
+                    <a href="browse-jobs.html">
+                        <div class="icon bg-color-2">
+                            <i class="lni-world"></i>
+                        </div>
+                        <h3>Sale/Markting</h3>
+                        <p>(2000 jobs)</p>
+                    </a>
                 </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div class="col-lg-3 col-md-6 col-xs-12 f-category">
+                    <a href="browse-jobs.html">
+                        <div class="icon bg-color-3">
+                            <i class="lni-book"></i>
+                        </div>
+                        <h3>Education/Training</h3>
+                        <p>(1450 jobs)</p>
+                    </a>
+                </div>
+                <div class="col-lg-3 col-md-6 col-xs-12 f-category">
+                    <a href="browse-jobs.html">
+                        <div class="icon bg-color-4">
+                            <i class="lni-display"></i>
+                        </div>
+                        <h3>Technologies</h3>
+                        <p>(5100 jobs)</p>
+                    </a>
+                </div>
+                <div class="col-lg-3 col-md-6 col-xs-12 f-category">
+                    <a href="browse-jobs.html">
+                        <div class="icon bg-color-5">
+                            <i class="lni-brush"></i>
+                        </div>
+                        <h3>Art/Design</h3>
+                        <p>(5079 jobs)</p>
+                    </a>
+                </div>
+                <div class="col-lg-3 col-md-6 col-xs-12 f-category">
+                    <a href="browse-jobs.html">
+                        <div class="icon bg-color-6">
+                            <i class="lni-heart"></i>
+                        </div>
+                        <h3>Healthcare</h3>
+                        <p>(3235 jobs)</p>
+                    </a>
+                </div>
+                <div class="col-lg-3 col-md-6 col-xs-12 f-category">
+                    <a href="browse-jobs.html">
+                        <div class="icon bg-color-7">
+                            <i class="lni-funnel"></i>
+                        </div>
+                        <h3>Science</h3>
+                        <p>(1800 jobs)</p>
+                    </a>
+                </div>
+                <div class="col-lg-3 col-md-6 col-xs-12 f-category">
+                    <a href="browse-jobs.html">
+                        <div class="icon bg-color-8">
+                            <i class="lni-cup"></i>
+                        </div>
+                        <h3>Food Services</h3>
+                        <p>(4286 jobs)</p>
+                    </a>
                 </div>
             </div>
         </div>
-    </body>
-</html>
+    </section>
+    <div id="browse-jobs" class="section bg-gray">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-md-12 col-sm-12">
+                    <div class="text-wrapper">
+                        <div>
+                            <h3>500+ talents</h3>
+                            <p>Search all the open positions on the web. Get your own personalized salary estimate. Read reviews on over 600,000 companies worldwide. The right job is out there.</p>
+                            <a class="btn btn-common" href="#">Search jobs</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-12 col-sm-12">
+                    <div class="img-thumb">
+                        <img class="img-fluid" src="assets/img/search.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <section class="how-it-works section">
+        <div class="container">
+            <div class="section-header">
+                <h2 class="section-title">How It Works?</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit ellentesque dignissim quam et <br> metus effici turac fringilla lorem facilisis.</p>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                    <div class="work-process">
+                        <span class="process-icon">
+                            <i class="lni-user"></i>
+                        </span>
+                        <h4>Create an Account</h4>
+                        <p>Post a job to tell us about your project. We'll quickly match you with the right freelancers find place best.</p>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                    <div class="work-process step-2">
+                        <span class="process-icon">
+                            <i class="lni-search"></i>
+                        </span>
+                        <h4>Search Jobs</h4>
+                        <p>Post a job to tell us about your project. We'll quickly match you with the right freelancers find place best.</p>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                    <div class="work-process step-3">
+                        <span class="process-icon">
+                            <i class="lni-cup"></i>
+                        </span>
+                        <h4>Apply</h4>
+                        <p>Post a job to tell us about your project. We'll quickly match you with the right freelancers find place best.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="testimonial" class="section">
+        <div class="container">
+            <div class="section-header">
+                <h2 class="section-title">Clients Review</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit ellentesque dignissim quam et <br> metus effici turac fringilla lorem facilisis.</p>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-lg-8 col-md-12 col-sm-12 col-xs-12">
+                    <div id="testimonials" class="touch-slider owl-carousel">
+                        <div class="item">
+                            <div class="testimonial-item">
+                                <div class="author">
+                                    <div class="img-thumb">
+                                        <img src="assets/img/testimonial/img1.png" alt="">
+                                    </div>
+                                </div>
+                                <div class="content-inner">
+                                    <p class="description">Morbi quam enim, cursus non erat pretium veh icula finibus ex stibulum venenatis viverra dui Morbi quam enim, cursus non erat pretium veh icula finibus ex stibulum venenatis viverra dui Morbi quam enim, cursus non erat pretium veh icula finibus ex stibulum venenatis viverra dui.</p>
+                                    <div class="author-info">
+                                        <h2><a href="#">Jessica</a></h2>
+                                        <span>Senior Accountant</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="testimonial-item">
+                                <div class="author">
+                                    <div class="img-thumb">
+                                        <img src="assets/img/testimonial/img2.png" alt="">
+                                    </div>
+                                </div>
+                                <div class="content-inner">
+                                    <p class="description">Morbi quam enim, cursus non erat pretium veh icula finibus ex stibulum venenatis viverra dui Morbi quam enim, cursus non erat pretium veh icula finibus ex stibulum venenatis viverra dui Morbi quam enim, cursus non erat pretium veh icula finibus ex stibulum venenatis viverra dui.</p>
+                                    <div class="author-info">
+                                        <h2><a href="#">John Doe</a></h2>
+                                        <span>Project Menager</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="testimonial-item">
+                                <div class="author">
+                                    <div class="img-thumb">
+                                        <img src="assets/img/testimonial/img3.png" alt="">
+                                    </div>
+                                </div>
+                                <div class="content-inner">
+                                    <p class="description">Morbi quam enim, cursus non erat pretium veh icula finibus ex stibulum venenatis viverra dui Morbi quam enim, cursus non erat pretium veh icula finibus ex stibulum venenatis viverra dui Morbi quam enim, cursus non erat pretium veh icula finibus ex stibulum venenatis viverra dui.</p>
+                                    <div class="author-info">
+                                        <h2><a href="#">Helen</a></h2>
+                                        <span>Engineer</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <div id="pricing" class="section bg-gray">
+        <div class="container">
+            <div class="section-header">
+                <h2 class="section-title">Pricing Plan</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit ellentesque dignissim quam et <br> metus effici turac fringilla lorem facilisis.</p>
+            </div>
+            <div class="row pricing-tables">
+                <div class="col-lg-4 col-md-4 col-xs-12">
+                    <div class="pricing-table border-color-defult">
+                        <div class="pricing-details">
+                            <div class="icon">
+                                <i class="lni-rocket"></i>
+                            </div>
+                            <h2>Professional</h2>
+                            <ul>
+                                <li>Post 1 Job</li>
+                                <li>No Featured Job</li>
+                                <li>Edit Your Job Listing</li>
+                                <li>Manage Application</li>
+                                <li>30-day Expired</li>
+                            </ul>
+                            <div class="price"><span>$</span>0<span>/Month</span></div>
+                        </div>
+                        <div class="plan-button">
+                            <a href="#" class="btn btn-border">Get Started</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-xs-12">
+                    <div class="pricing-table pricing-active border-color-red">
+                        <div class="pricing-details">
+                            <div class="icon">
+                                <i class="lni-drop"></i>
+                            </div>
+                            <h2>Advance</h2>
+                            <ul>
+                                <li>Post 1 Job</li>
+                                <li>No Featured Job</li>
+                                <li>Edit Your Job Listing</li>
+                                <li>Manage Application</li>
+                                <li>30-day Expired</li>
+                            </ul>
+                            <div class="price"><span>$</span>20<span>/Month</span></div>
+                        </div>
+                        <div class="plan-button">
+                            <a href="#" class="btn btn-border">Get Started</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-xs-12">
+                    <div class="pricing-table border-color-green">
+                        <div class="pricing-details">
+                            <div class="icon">
+                                <i class="lni-briefcase"></i>
+                            </div>
+                            <h2>Premium</h2>
+                            <ul>
+                                <li>Post 1 Job</li>
+                                <li>No Featured Job</li>
+                                <li>Edit Your Job Listing</li>
+                                <li>Manage Application</li>
+                                <li>30-day Expired</li>
+                            </ul>
+                            <div class="price"><span>$</span>40<span>/Month</span></div>
+                        </div>
+                        <div class="plan-button">
+                            <a href="#" class="btn btn-border">Get Started</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
