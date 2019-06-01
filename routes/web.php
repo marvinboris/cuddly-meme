a2ef4ad403740929bf2ac37f326a956380cd3d02
sdmg15 <sonkengmaldini@gmail.com>
2019-06-01 05:45:24 +0100

    Adding logging view

--- routes/web.php
+++ routes/web.php
@@ -11,14 +11,14 @@
 |
 */
 
-
 Route::get('/', 'FrontEndController@home')->name('home');
+
+Route::get('/sysadmin/logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
 
 Route::post('ajax-get-cities-by-country', 'FrontEndController@ajaxGetCitiesByCountry')->name('ajax-get-cities-by-country');
 Route::post('ajax-search-cities', 'FrontEndController@ajaxSearchCities')->name('ajax-search-cities');
 
-Route::match(['get','post'],'search', 'FrontEndController@searchWorker')->name('search-worker');
-
+Route::match(['get', 'post'],'search', 'FrontEndController@searchWorker')->name('search-worker');
 
 Route::get('register', 'FrontEndController@register')->name('register');
 Route::post('register', 'FrontEndController@postRegister');
@@ -28,22 +28,17 @@ Route::get('login', 'AuthController@login')->name('login');
 Route::post('login', 'AuthController@postSignin');
 Route::get('logout', 'AuthController@getLogout')->name('logout');
 
-
-# Account Activation
+// Account Activation
 Route::get('activate/{userId}/{activationCode}', 'AuthController@getActivate')->name('activate');
 
-
 Route::get('payment', 'FrontEndController@payment')->name('payment')->middleware('user');
-
 
 Route::group(['middleware' => 'has-paid'], function () {
     Route::get('dashboard', 'AuthController@dashboard')->name('dashboard');
 });
 
-
-Route::group(['prefix' => 'admin', 'namespace'=>'Admin', 'as' => 'admin.'], function () {
-
-    # Error pages should be shown without requiring login
+Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'as' => 'admin.'], function () {
+    // Error pages should be shown without requiring login
     Route::get('404', function () {
         return view('admin/404');
     });
@@ -51,29 +46,25 @@ Route::group(['prefix' => 'admin', 'namespace'=>'Admin', 'as' => 'admin.'], func
         return view('admin/500');
     });
 
-    # All basic routes defined here
+    // All basic routes defined here
     Route::get('login', 'AuthController@getSignin')->name('login');
     Route::get('signin', 'AuthController@getSignin')->name('signin');
     Route::post('signin', 'AuthController@postSignin')->name('postSignin');
     Route::post('signup', 'AuthController@postSignup')->name('admin.signup');
     Route::post('forgot-password', 'AuthController@postForgotPassword')->name('signup');
 
-    # Forgot Password Confirmation
+    // Forgot Password Confirmation
     Route::get('forgot-password/{userId}/{passwordResetCode}', 'AuthController@getForgotPasswordConfirm')->name('forgot-password-confirm');
     Route::post('forgot-password/{userId}/{passwordResetCode}', 'AuthController@getForgotPasswordConfirm');
 
-    # Logout
+    // Logout
     Route::get('logout', 'AuthController@getLogout')->name('logout');
 });
 
-
-
-
-Route::group(['prefix' => 'admin', 'namespace'=>'Admin' , 'as' => 'admin.', 'middleware' => 'admin'], function () {
+Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'as' => 'admin.', 'middleware' => 'admin'], function () {
     Route::get('/', 'AuthController@dashboard')->name('dashboard');
     Route::get('home', 'AuthController@dashboard');
     Route::get('dashboard', 'AuthController@dashboard');
-
 
     Route::group(['prefix' => 'users', 'as' => 'users.'], function () {
         Route::get('trashed', 'UsersController@trashed')->name('trashed');
@@ -99,7 +90,5 @@ Route::group(['prefix' => 'admin', 'namespace'=>'Admin' , 'as' => 'admin.', 'mid
 
     Route::get('transactions', 'TransactionsController@transactions')->name('transactions');
 });
-
-
 
 Route::get('{link}', 'FrontEndController@userLink')->name('user-link');
