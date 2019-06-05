3efc8cbbeded4826a86b5c9565bfb1156a5fc3c0
sdmg15 <sonkengmaldini@gmail.com>
2019-06-06 00:03:46 +0100

    Fixing fa icon

--- resources/views/partials/layout.blade.php
+++ resources/views/partials/layout.blade.php
@@ -37,7 +37,7 @@
         <link rel="stylesheet" href="{{ asset('css/animate.css') }}" >
         <link rel="stylesheet" href="{{ asset('css/main.css') }}" >
         <link rel="stylesheet" href="{{ asset('css/responsive.css') }}" >
-        <link rel="stylesheet" href="{{ asset('css/font-awesome.min.css') }}" >
+        <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
         @yield("stylesheet")
 
     </head>
